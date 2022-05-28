633566351988dd12df50a99a59f5050ca8ad8031
aishwarya4658 <33395864+aishwarya4658@users.noreply.github.com>
2022-05-28 22:09:13 +0530

    Time: 10 ms (42.23%), Space: 46.6 MB (36.46%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character ,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int res=0;
+
+        for(int i=0;i<s.length()-1;i++){
+
+            int curr=map.get(s.charAt(i));
+            int next=map.get(s.charAt(i+1));
+            res=curr<next?res-curr:res+curr;
+        }
+
+        //AS it goes only upto second last position
+        return res+ map.get(s.charAt(s.length()-1));
+    }
+}
+        
+    
