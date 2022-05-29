4eb4fc087a2c1bbde7b7c42063b38333eabafce3
aishwarya4658 <33395864+aishwarya4658@users.noreply.github.com>
2022-05-30 02:55:53 +0530

    Time: 0 ms (100.00%), Space: 43 MB (90.55%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int expected = nums.length*(nums.length+1)/2;
+        int actual= 0;
+        for(int num:nums) 
+            actual+=num;
+        return expected-actual;
+    }
+}
